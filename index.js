3216c253a7d8211da29e028d8637ba6c5370336a
gbroadhead <gwbroadhead@gmail.com>
2021-12-19 20:40:55 -0800

    answers switch created with logs

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-const mysql = require('mysql2')
 const inquirer = require('inquirer');
 const { prompt } = require("inquirer");
 const consoleTable = require('console.table');
-const AddEntriesFromIterable = require('es-abstract/2019/AddEntriesFromIterable');
-const { allowedNodeEnvironmentFlags } = require('process');
+const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: "localhost",
   user: 'root',
@@ -15,59 +13,85 @@ const start = () => {
   inquirer.prompt([
     {
       type: 'list',
-      name: 'todo',
-      message: "What would you like to do?"
-    choices: ['Add Department(s)', 'Add Role(s)', 'Add Employee(s)', 'View Department(s)', 'View Role(s)', 'View Employee(s)', 'Update Employee Role(s)', 'Update Employee Manager(s)', 'View Employee(s) by Manager', 'Remove Department(s)', 'Remove Role(s)', 'Remove Employee(s)', 'View Total Untilized Budget of Department(s)' 'Leave']
+      message: "What would you like to do?",
+      choices: ['Add Department(s)', 'Add Role(s)', 'Add Employee(s)', 'View Department(s)', 'View Role(s)', 'View Employee(s)', 'Update Employee Role(s)', 'Update Employee Manager(s)', 'View Employee(s) by Manager', 'Remove Department(s)', 'Remove Role(s)', 'Remove Employee(s)', 'View Total Utilized Budget', 'Leave'],
+      name: 'todo'
     },
   ])
-    .then(({ answers }) => {
-      switch (answers) {
+    .then((answers) => {
+      switch (answers.todo) {
         case 'Add Department(s)':
-          addDepartments();
+          // addDepartments();
+          console.log(answers.todo)
           break;
 
         case 'Add Role(s)':
-          addRoles();
+          // addRoles();
+          console.log(answers.todo)
           break;
 
         case 'Add Employee(s)':
-          addEmployees();
+          // addEmployees();
+          console.log(answers.todo)
           break;
 
         case 'View Department(s)':
-          viewDepartments();
+          // viewDepartments();
+          console.log(answers.todo)
           break;
 
         case 'View Role(s)':
-          viewRoles();
+          // viewRoles();
+          console.log(answers.todo)
           break;
 
         case 'View Employees(s)':
-          viewEmployees();
+          // viewEmployees();
+          console.log(answers.todo)
           break;
 
         case 'Update Employee Role(s)':
-          updateRoles();
+          // updateRoles();
+          console.log(answers.todo)
           break;
 
         case 'Update Employee Manager(s)':
-          updateManagers();
+          // updateManagers();
+          console.log(answers.todo)
           break;
 
         case 'View Employee(s) by Manager':
-          viewManagers();
+          // viewManagers();
+          console.log(answers.todo)
           break;
 
         case 'Remove Department(s)':
-          removeDepartments();
+          // removeDepartments();
+          console.log(answers.todo)
           break;
 
-        case 'Remove Department(s)':
-          removeDepartments();
+        case 'Remove Roles(s)':
+          // removeRoles();
+          console.log(answers.todo)
+          break;
+
+        case 'Remove Employee(s)':
+          // removeEmployees();
+          console.log(answers.todo)
+          break;
+
+        case 'View Total Utilized Budget':
+          // viewBudget();
+          console.log(answers.todo)
+          break;
+
+        case 'Leave':
+          // leave();
+          console.log(answers.todo)
           break;
 
       }
+    })
+}
 
-    }
-
-    )
+start();
