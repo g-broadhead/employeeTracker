eb3fcb26a10bac8ac205ed3ce5114e6f4068f2ca
gbroadhead <gwbroadhead@gmail.com>
2021-12-19 21:58:33 -0800

    created functions for add and view

--- index.js
+++ index.js
@@ -21,32 +21,32 @@ const start = () => {
     .then((answers) => {
       switch (answers.todo) {
         case 'Add Department(s)':
-          // addDepartments();
+          addDepartments();
           console.log(answers.todo)
           break;
 
         case 'Add Role(s)':
-          // addRoles();
+          addRoles();
           console.log(answers.todo)
           break;
 
         case 'Add Employee(s)':
-          // addEmployees();
+          addEmployees();
           console.log(answers.todo)
           break;
 
         case 'View Department(s)':
-          // viewDepartments();
+          viewDepartments();
           console.log(answers.todo)
           break;
 
         case 'View Role(s)':
-          // viewRoles();
+          viewRoles();
           console.log(answers.todo)
           break;
 
         case 'View Employees(s)':
-          // viewEmployees();
+          viewEmployees();
           console.log(answers.todo)
           break;
 
@@ -94,4 +94,110 @@ const start = () => {
     })
 }
 
-start();
+start()
+
+
+// View Functions
+function viewDepartments() {
+  db.query('SELECT * FROM department', (err, department) => {
+    if (err) { console.log(err) }
+    console.table(department)
+    start()
+  })
+}
+
+function viewRoles() {
+  db.query('SELECT * FROM roles', (err, roles) => {
+    if (err) { console.log(err) }
+    console.table(roles)
+    start()
+  })
+}
+
+function viewEmployees() {
+  db.query('SELECT * FROM employee', (err, employee) => {
+    if (err) { console.log(err) }
+    console.table(employee)
+    start()
+  })
+}
+
+function viewManagers() {
+  db.query('SELECT * FROM employee WHERE ? manager_id', (err, employee) => {
+    if (err) { console.log(err) }
+    console.table(employee)
+    start()
+  })
+}
+
+// Add Functions
+function addDepartments() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Name of new department.'
+    }
+  ])
+    .then(newDepartment => {
+      db.query('INSERT INTO department SET ?', newDepartment, err => {
+        if (err) { console.log(err) }
+        console.log(`${newDepartment} has been added!`)
+        start()
+      })
+    })
+}
+
+function addRoles() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Name of new role.'
+    },
+    {
+      type: 'input',
+      name: 'department_id',
+      message: 'What is the department ID?'
+    },
+    {
+      type: 'input',
+      name: 'salary',
+      message: `What is the new role's salary?`
+    }
+  ])
+    .then(newRole => {
+      db.query('INSERT INTO roles SET ?', newRole, err => {
+        if (err) { console.log(err) }
+        console.log(`${newRole} has been added!`)
+        start()
+      })
+    })
+}
+
+function addEmployees() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'first_name',
+      message: 'What is the FIRST name of the employee?'
+    },
+    {
+      type: 'input',
+      name: 'last_name',
+      message: 'What is the LAST name of the employee?'
+    },
+    {
+      type: 'list',
+      name: 'roles_id',
+      message: `What is the new employee's role ID?`
+    }
+  ])
+    .then(newEmployee => {
+      db.query('INSERT INTO employee SET ?', newEmployee, err => {
+        if (err) { console.log(err) }
+        console.log(`${newEmployee} has been added!`)
+        start()
+      })
+    })
+}
